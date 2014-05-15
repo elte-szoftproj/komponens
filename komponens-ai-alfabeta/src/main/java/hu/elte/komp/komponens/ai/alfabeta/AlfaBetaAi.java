278b23a23789992d39c2424cc82862a983007e1d
davidangeli <david.angeli@outlook.com>
2014-05-15 06:45:13 +0200

    alfabeta ai

---
+++ komponens-ai-alfabeta/src/main/java/hu/elte/komp/komponens/ai/alfabeta/AlfaBetaAi.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.elte.komp.komponens.ai.alfabeta;
+
+import hu.elte.komp.game.AiInterface;
+import hu.elte.komp.game.GameGraphInterface;
+import java.util.ArrayList;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author martin
+ */
+@Stateless(name="hu.elte.komp.ai.alfabeta", mappedName = "hu.elte.komp.ai.alfabeta")
+@LocalBean
+public class AlfaBetaAi implements AiInterface {
+    private static final int MELYSEG=2;
+
+    @Override
+    public Object getNextStep(GameGraphInterface gameGraph, boolean youArePlayerOne) {
+        
+        long max=Long.MIN_VALUE;
+        Object nextstep=null;
+        ArrayList<Long> nextsteps=new ArrayList();
+        
+        for(Object o: gameGraph.getPossibleSteps(gameGraph.getCurrentStep())) {
+            long l=alfabeta(gameGraph,o,MELYSEG,false,Long.MIN_VALUE,Long.MAX_VALUE);
+            nextsteps.add(l);
+            if(l>=max) {
+                max=l;
+                nextstep=o;           
+            }
+        }
+        
+        if (max>0){
+            int a=1;
+        }
+        // ha keveset lephetunk, es tul nagy random? feladjuk...
+        return nextstep;        
+    }
+    
+    private long alfabeta (GameGraphInterface gameGraph, Object node, int m, boolean maximizing, long alpha, long beta){
+        long al=alpha, be=beta;
+        //ha level, erteket kap
+        if (m==0 || !gameGraph.getPossibleSteps(node).iterator().hasNext()){
+          return gameGraph.getStepScoreForPlayer(node, false);
+        }
+        
+        if (maximizing){
+            for(Object o: gameGraph.getPossibleSteps(node)){
+                be = Math.min(be, alfabeta(gameGraph, o, m-1, !maximizing, al, be));
+                if (be<=al) return al;
+            }
+            return be;
+        }
+        else {
+            for(Object o: gameGraph.getPossibleSteps(node)){
+                al = Math.max(al, alfabeta(gameGraph, o, m-1, !maximizing, al, be));
+                if (be<=al) return be;
+            }
+            return al; 
+        }
+   }
+
+}
+

177e876b0764bcde5c629d401deaf4c109945fd8
davidangeli <david.angeli@outlook.com>
2014-05-20 18:34:48 +0200

    modified simple scorecalculator

--- kompgame-game-kamisado/src/main/java/hu/elte/komp/kompgame/game/kamisado/WinCalculator.java
+++ kompgame-game-kamisado/src/main/java/hu/elte/komp/kompgame/game/kamisado/WinCalculator.java
@@ -8,17 +8,27 @@ package hu.elte.komp.kompgame.game.kamisado;
 
 import hu.elte.komp.game.ScoreCalculator;
 
+/**
+ * Heurisztika:: nyertes állás: 100 pont, vesztes: 0, egyébként:50.
+ * {@link hu.elte.komp.game.ScoreCalculator ScoreCalculator} interface.
+ * @author angelid
+ */
 public class WinCalculator implements ScoreCalculator {
 
     @Override
     public long getScoreForStep(Object step) {
         KamisadoAiIterator.StepInfo si=(KamisadoAiIterator.StepInfo) step;
         for (int i=0;i<8;i++) {
+            //nyertes állás, 100
             if (si.board.charAt(63-i) >= 'i' && si.board.charAt(63-i) <= 'p') {
                     return 100;
             }
+            //vesztes állás, 0
+            if (si.board.charAt(i) >= 'a' && si.board.charAt(i) <= 'h') {
+                return 0;
+            }            
         }
-        return 0;
+        return 50;
     }
     
 }
